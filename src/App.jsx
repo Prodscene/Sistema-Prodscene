0137279f1816a1bb06be7a0f2c68c01d59c7f3e9
Prodscene <ealvescontato@gmail.com>
2025-04-02 19:07:49 -0300

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>Prodscene está no ar! 🎬</h1>
-      <p>Seu MVP foi publicado com sucesso via Vercel + GitHub.</p>
-    </div>
-  );
-}
